807537874f6636e25c1b2688e8e74fe217b90be2
Istiak <mhistiak2@gmail.com>
2024-09-08 22:07:55 +0600

    Install react using vite

---
+++ 01-vite-react/src/App.jsx
@@ -0,0 +1,12 @@
+import Wc from "./Wc";
+
+function App() {
+  return (
+    <div>
+      <h2>Hello Javascript Developer</h2>
+      <Wc />
+    </div>
+  );
+}
+
+export default App;
---
+++ 01-vite-react/src/Wc.jsx
@@ -0,0 +1,4 @@
+function Wc() {
+    return <h3>Welcome To The World of ReactJS</h3>
+}
+export default Wc
